5ed586cfe606fc285ffa8a3c3a319eeaceb9417e
dwhiteBCIT <dwhite170@my.bcit.ca>
2026-04-09 17:59:36 -0700

    got /show/:postid to work

--- routers/postRouters.ts
+++ routers/postRouters.ts
@@ -23,18 +23,13 @@ router.post("/create", ensureAuthenticated, async (req, res) => {
 
 router.get("/show/:postid", async (req, res) => {
   // ⭐ TODO
-  //   shows post title, post link, timestamp, and creator
-  // also has a list of all comments related to this post
-  // each of these should show the comment description, creator, and timestamp
-  // optionally, each comment could have a link to delete it
-  // if you're logged in, a form for commenting should show
   // req.params.postid
-  let posts = await database.getPosts(1, req.params.postid);
-  posts = posts.map((post) => {
-    return { ...post, creator: database.getUser(post.creator) };
-  });
+  let postids = await database.getPost(req.params.postid); // gets specifc post
+  // posts = posts.map((post) => {
+  //   return { ...post, creator: database.getUser(post.creator) };
+  // });
   const user = await req.user;
-  res.render("individualPost", { posts });
+  res.render("individualPost", { postids, user });
 });
 
 router.get("/edit/:postid", ensureAuthenticated, async (req, res) => {
